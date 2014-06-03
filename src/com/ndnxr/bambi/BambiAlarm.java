15fba62fb0339bccf46cf6dd7042cce00c772b84
Ivan Yeo <ivanyeo@ucla.edu>
2014-06-02 19:18:00 -0700

    Remove BambiAlarm.java and consolidate all messages in BambiMessages.java.

--- src/com/ndnxr/bambi/BambiAlarm.java
+++
@@ -1,9 +0,0 @@
-package com.ndnxr.bambi;
-
-public class BambiAlarm {
-	// Alarm Message: Key values used for Intent Messages
-	public static final String MESSAGE_ALARM = "ALARM_MESSAGE";
-	
-	// Alarm Messages
-	public static final int MESSAGE_ALARM_ARRIVED = 1;
-}
---
+++ src/com/ndnxr/bambi/BambiMessages.java
@@ -0,0 +1,24 @@
+package com.ndnxr.bambi;
+
+public class BambiMessages {
+	// Alarm Message: Key values used for Intent Messages
+	public static final String MESSAGE_ALARM = "ALARM_MESSAGE";
+
+	// Alarm Messages
+	public static final int MESSAGE_ALARM_ARRIVED = 1;
+
+	
+	// Boot Complete Message: Key values used for Intent Messages
+	public static final String MESSAGE_BOOT_COMPLETE = "BOOT_COMPLETE_MESSAGE";
+
+	// Boot Complete Messages
+	public static final int MESSAGE_BOOT_COMPLETE_ARRIVED = 2;
+
+	
+	// Wifi Connected Message: Key values used for Intent Messages
+	public static final String MESSAGE_WIFI_CONNECTED = "WIFI_CONNECTED_MESSAGE";
+
+	// Wifi Connected Messages
+	public static final int MESSAGE_WIFI_CONNECTED_ARRIVED = 3;
+
+}
