f566038e7ebc32173ba0cd94eabb39cc881d4077
Ivan Yeo <ivanyeo@ucla.edu>
2014-06-03 04:16:38 -0700

    Update BootCompleteReceiver to start the BambiEnergyService and process the scheduleTask list.

--- src/com/ndnxr/bambi/BambiBootCompleteReceiver.java
+++ src/com/ndnxr/bambi/BambiBootCompleteReceiver.java
@@ -7,14 +7,23 @@ import android.content.Intent;
 public class BambiBootCompleteReceiver extends BroadcastReceiver {
 
 	@Override
-	public void onReceive(Context arg0, Intent arg1) {
+	public void onReceive(Context context, Intent intent) {
 		G.Log("BambiBootCompleteReceiver::onReceive()");
 		
-		// TODO startService() and do these in the Service
+		// startService() and do these in the Service
 		/*		(1) Read in Tasks from Files (done in service)
 		 *  	(2) Send anything that is past due
 		 *  	(3) Set Alarm for SCHEDULE Tasks in the future as AlarmManager gets flushed on re-boot
 		 */
+
+		// Create service intent
+		Intent serviceIntent = new Intent(context, BambiEnergyService.class);
+
+		// Append Message
+		serviceIntent.putExtra(BambiMessages.MESSAGE_BOOT_COMPLETE, BambiMessages.MESSAGE_BOOT_COMPLETE_ARRIVED);
+
+		// Start service
+		context.startService(serviceIntent);
 	}
 
 }
