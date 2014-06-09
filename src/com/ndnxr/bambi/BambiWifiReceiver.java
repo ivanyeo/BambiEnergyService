913968074c6a8138f0fdc1cb26a4e5c0923129a2
Ivan Yeo <ivanyeo@ucla.edu>
2014-06-09 03:33:16 -0700

    Send Wifi disconnection message from the BambiWifiReceiver to the BambiEnergyService.

--- src/com/ndnxr/bambi/BambiWifiReceiver.java
+++ src/com/ndnxr/bambi/BambiWifiReceiver.java
@@ -31,7 +31,29 @@ public class BambiWifiReceiver extends BroadcastReceiver {
 	    		
 	        } else {
 	        	G.Log("BambiWifiReceiver: Wifi Disonnected!");
-	            // Wifi Disconnected - Nothing needs to be done
+	            // Wifi Disconnected
+	        	
+	        	/*
+	        	 * Because Android does not provide an API to check if a service is running,
+	        	 * we have to send a message to the service in order to take care of the case
+	        	 * when the Service is already running and the Wifi connection was lost.
+	        	 * 
+	        	 * This allows BambiEnergyService to update it's internal boolean variable wifiConnected
+	        	 * if it is running so as to not allow Tasks to make use of the Wifi connection.
+	        	 * 
+	        	 * Reference:
+	        	 * http://stackoverflow.com/questions/600207/how-to-check-if-a-service-is-running-in-android
+	        	 * https://groups.google.com/forum/#!topic/android-developers/jEvXMWgbgzE
+	        	 */
+	        	
+	    		// Create service intent
+	    		Intent serviceIntent = new Intent(context, BambiEnergyService.class);
+
+	    		// Append Message
+	    		serviceIntent.putExtra(BambiMessages.MESSAGE_WIFI_DISCONNECTED, BambiMessages.MESSAGE_WIFI_DISCONNECTED_ARRIVED);
+
+	    		// Start service
+	    		context.startService(serviceIntent);	        	
 	        }
 	    }
 	}
