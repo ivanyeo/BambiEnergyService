41f4daeb2f9d10e0e8fc0c8f785de98da33c3f78
Ivan Yeo <ivanyeo@cs.ucla.edu>
2014-06-10 20:39:57 -0700

    Implement Wifi retry timeout.

--- src/com/ndnxr/bambi/G.java
+++ src/com/ndnxr/bambi/G.java
@@ -6,4 +6,16 @@ public class G {
 	public static void Log(String msg) {
 		android.util.Log.d(TAG, msg);
 	}
+	
+	/**
+	 *  Timeout for checking Wifi connection in BambiEnergyService::processTask()
+	 *  in milliseconds. 
+	 */
+	public static final int WIFI_RETRY_TIMEOUT = 200;
+	
+	/**
+	 * Number of times that the Wifi connection should be probed before being
+	 * considered non-active.
+	 */
+	public static final int WIFI_RETRY_COUNT = 8;
 }
