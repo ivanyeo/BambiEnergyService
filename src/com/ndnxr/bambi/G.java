991bccc38e5a217ad4a767d78bf3ed1be8f71f94
Ivan Yeo <ivanyeo@cs.ucla.edu>
2014-06-10 22:17:59 -0700

    Implement new message keys.

--- src/com/ndnxr/bambi/G.java
+++ src/com/ndnxr/bambi/G.java
@@ -18,4 +18,18 @@ public class G {
 	 * considered non-active.
 	 */
 	public static final int WIFI_RETRY_COUNT = 8;
+	
+	/**
+	 * Preference file that is private to BambiEnergyService.
+	 */
+	public static final String ENERGY_SERVICE_PREFERENCE_FILE_KEY = "com.ndnxr.bambienergyservice.SERVICE_PREFERENCE_FILE";
+	
+	/**
+	 * Key for the total number of bytes that passed through BambiEnergyService
+	 */
+	public static final String ENERGY_SERVICE_TOTAL_BYTES_PASSED_THROUGH = "com.ndnxr.bambienergyservice.TOTAL_BYTES_PASSED_THROUGH";
+	
+	
+	
+	
 }
