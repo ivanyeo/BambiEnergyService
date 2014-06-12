27f7bf68d498d536d1e2e8c0f2c71ee50e2cab3f
Monica <qirongismonica@gmail.com>
2014-06-12 01:00:05 -0700

    Update fragment code and fix bug of duplicate EnergyChart.

--- src/com/ndnxr/bambi/EnergyMeterFragment.java
+++ src/com/ndnxr/bambi/EnergyMeterFragment.java
@@ -2,6 +2,7 @@ package com.ndnxr.bambi;
 import com.ndnxr.bambi.R;
 
 import android.animation.ValueAnimator;
+import android.app.Activity;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -21,69 +22,98 @@ public class EnergyMeterFragment extends Fragment {
 	private int screen_height;
 	private LinearLayout rootViewLayout ;
 	private TextView textView;
+	private int energySave;
+	private long totalDataBytes;
 	
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
     	
-    	// Initialization
-        final View rootView = inflater.inflate(R.layout.fragment_energy_meter, container, false);
+    	G.Log("onCreateView() !!");
+    	
+    	// Set basic root view layout
+        View rootView = inflater.inflate(R.layout.fragment_energy_meter, container, false);
         rootViewLayout = (LinearLayout) rootView;
         
-        
-        // Create energy chart
-        energyChart = new EnergyChart();
-        energyChartView = energyChart.setEmptyView(this.getActivity());
-        
-        // Get screen size
+        // Get screen size for display pie chart
         Display display = this.getActivity().getWindowManager().getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
         screen_width = size.x;
         screen_height = size.y;
         
-        // Add energy chart to root view
-        G.Log(String.format("Height is: %d", screen_height) );
-        rootViewLayout.addView(energyChartView, screen_width, screen_height/2);
+        // Update energySaveRaio and total data byte from Bambi
+        totalDataBytes = ((Bambi)this.getActivity()).totalDataBytes;
+        energySave = ((Bambi) this.getActivity()).energySavePercent;
+        recreateRootView();
         
+        /*
+        // Get energy value from Bambi to create pie chart
+        createEnergySaveChart(((Bambi) this.getActivity()).energySavePercent);
         
-        // Add text info to root view
+        // Get total byte value from Bambi to write textView      
         textView = new TextView(this.getActivity());
-        textView.setText("Bambi Saved Your Battary!");
+        totalDataBytes = ((Bambi)this.getActivity()).totalDataBytes;
+        textView.setText(String.format("Bambi Uploaded %d bytes of Data in total",totalDataBytes));
         textView.setGravity(Gravity.CENTER_HORIZONTAL);
-
+        
+        // Add views to root view
+        rootViewLayout.addView(energyChartView,screen_width, screen_height/2);
         rootViewLayout.addView(textView);
-           
+        */
+       
+      
         return rootView;
     }
     
     /**
-     * Update energy save percentage in pie chart
+     * create energy save percentage in pie chart
      * @return 
      */
-    public void updateEnergySave(int energysave){
-    	rootViewLayout.removeView(energyChartView);
-    	energyChartView = energyChart.setView(this.getActivity(), energysave);
-    	updateRootView();
-    	//rootViewLayout.requestLayout();    	
+    public void createEnergySaveChart(int energysave){
+    	G.Log("drawEnergySaveChart called "+energysave);
+    	// Create energy chart
+        energyChart = new EnergyChart();
+        // Create energyChartView with energy save ratio
+     	energyChartView = energyChart.setView(this.getActivity(), energysave);
     }
+    
+   
+    /**
+     * recreate the entire root view, called when update occurs
+     */
+    public void recreateRootView(){
+        	
+    	// Create energy chart
+        EnergyChart updateEnergyChart = new EnergyChart();
+        
+        // Create energyChartView with energy save ratio
+     	View updateEnergyChartView = updateEnergyChart.setView(this.getActivity(), energySave);
+     	
+     	// Create textView with totalDataBytes
+    	TextView updateTextView = new TextView(this.getActivity());
+    	updateTextView.setText(String.format("Bambi Uploaded %d bytes of Data in total",totalDataBytes) );
+    	updateTextView.setGravity(Gravity.CENTER_HORIZONTAL);
+    	// Clear out of date views element
+    	rootViewLayout.removeAllViews();
+     	
+    	// Add views to root view
+    	rootViewLayout.addView(updateEnergyChartView,screen_width, screen_height/2);
+    	rootViewLayout.addView(updateTextView);
+    }
+    
     
     /**
      * Update text view to display total Bytes number
      * @param totalBytes
      */
     public void updateTotalBytes(long totalBytes){
-    	textView.setText(String.format("Bambi Uploaded %d bytes of Data in total",totalBytes) );
-    	updateRootView();		
+    	totalDataBytes = totalBytes;
+    	recreateRootView();
     }
     
-    /**
-     * Update every widget
-     */
-    public void updateRootView(){
-    	rootViewLayout.removeView(energyChartView);
-    	rootViewLayout.removeView(textView);
-    	rootViewLayout.addView(energyChartView,screen_width, screen_height/2);
-    	rootViewLayout.addView(textView);
+    public void updateEnergySaveChart(int energysave){
+    	energySave = energysave;
+    	recreateRootView();    	
     }
 }
