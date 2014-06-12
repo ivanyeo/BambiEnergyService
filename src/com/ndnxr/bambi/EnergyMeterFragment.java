4dfb0ae58fa57dcbc2b926f15ece3b005ed784ab
Monica <qirongismonica@gmail.com>
2014-06-11 19:13:59 -0700

    Update view for EnergyMeterFragment.

--- src/com/ndnxr/bambi/EnergyMeterFragment.java
+++ src/com/ndnxr/bambi/EnergyMeterFragment.java
@@ -6,6 +6,7 @@ import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.Display;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,35 +15,75 @@ import android.widget.TextView;
  
 public class EnergyMeterFragment extends Fragment {
  
+	private View energyChartView;
+	private EnergyChart energyChart;
+	private int screen_width;
+	private int screen_height;
+	private LinearLayout rootViewLayout ;
+	private TextView textView;
+	
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
     	
     	// Initialization
         final View rootView = inflater.inflate(R.layout.fragment_energy_meter, container, false);
-        LinearLayout rootViewLayout = (LinearLayout) rootView;
+        rootViewLayout = (LinearLayout) rootView;
+        
         
         // Create energy chart
-        EnergyChart energyChart = new EnergyChart();
-        View energyChartView = energyChart.getView(this.getActivity());
+        energyChart = new EnergyChart();
+        energyChartView = energyChart.setEmptyView(this.getActivity());
         
         // Get screen size
         Display display = this.getActivity().getWindowManager().getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
-        int width = size.x;
-        int height = size.y;
+        screen_width = size.x;
+        screen_height = size.y;
         
         // Add energy chart to root view
-        G.Log(String.format("Height is: %d", height) );
-        rootViewLayout.addView(energyChartView, width, height/2);
+        G.Log(String.format("Height is: %d", screen_height) );
+        rootViewLayout.addView(energyChartView, screen_width, screen_height/2);
         
         
         // Add text info to root view
-        TextView textView = new TextView(this.getActivity());
-        textView.setText("This is my text!");
+        textView = new TextView(this.getActivity());
+        textView.setText("Bambi Saved Your Battary!");
+        textView.setGravity(Gravity.CENTER_HORIZONTAL);
+
         rootViewLayout.addView(textView);
            
         return rootView;
+    }
+    
+    /**
+     * Update energy save percentage in pie chart
+     * @return 
+     */
+    public void updateEnergySave(int energysave){
+    	rootViewLayout.removeView(energyChartView);
+    	energyChartView = energyChart.setView(this.getActivity(), energysave);
+    	updateRootView();
+    	//rootViewLayout.requestLayout();    	
+    }
+    
+    /**
+     * Update text view to display total Bytes number
+     * @param totalBytes
+     */
+    public void updateTotalBytes(long totalBytes){
+    	textView.setText(String.format("Bambi Uploaded %d bytes of Data in total",totalBytes) );
+    	updateRootView();		
+    }
+    
+    /**
+     * Update every widget
+     */
+    public void updateRootView(){
+    	rootViewLayout.removeView(energyChartView);
+    	rootViewLayout.removeView(textView);
+    	rootViewLayout.addView(energyChartView,screen_width, screen_height/2);
+    	rootViewLayout.addView(textView);
     }
 }
